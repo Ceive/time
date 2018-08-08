a8de90998b2ca46bd68a9d87d0936a092df1794f
lexus27 <lexus27.khv@gmail.com>
2018-08-08 17:07:19 +1000

    add: invertMonthIdentifier($monthId, $type = null)

---
+++ tests/BasicTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @Creator Alexey Kutuzov <lexus27.khv@gmail.com>
+ * @Author: Alexey Kutuzov <lexus27.khv@gmai.com>
+ * @Project: ceive.time
+ */
+
+namespace Ceive\Time\Tests;
+
+
+class BasicTest{
+	
+	public function testBasic(){
+		
+	}
+	
+}
+
+
